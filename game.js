034804581024d3d639c6d886998dd04d5e0342da
Mattias Erming <mattias@mattiaserming.com>
2014-06-23 23:57:47 +0200

    Minor changes

--- game.js
+++ game.js
@@ -1,29 +1,20 @@
-var rows = 24;
-var cols = 32;
-
-var game = null;
+var rows  = 24;
+var cols  = 32;
 var tiles = [];
 var timer = null;
 var speed = 100;
 
 $(function() {
-	game = $("#game");
-	refresh();
-	
 	var drag = false;
-	game.mousedown(function(e) {
-		drag = true;
-	}).on("mouseup mouseleave", function() {
-		drag = false;
-	}).on("mousedown mouseover", "td", function(e) {
+	var game = $("#game");
+	game.mousedown(function() { drag = true; });
+	game.on("mouseup mouseleave", function() { drag = false; });
+	game.on("mousedown mouseover", "td", function(e) {
 		e.preventDefault();
-		if (!drag && e.type != "mousedown") {
-			return;
+		if (drag || e.type == "mousedown") {
+			var td = $(this).toggleClass("alive");
 		}
-		var td = $(this);
-		td.toggleClass("alive");
 	});
-	
 	var buttons = $.each([
 		"play",
 		"fastForward",
@@ -33,6 +24,7 @@ $(function() {
 	], function(i, fn) {
 		$("#" + fn).on("click", window[fn]);
 	});
+	refresh();
 });
 
 function play() {
@@ -52,28 +44,23 @@ function fastForward() {
 }
 
 function step() {
-	var total = rows * cols;
-	for (var i = 0; i < total; i++) {
+	var size = rows * cols;
+	for (var i = 0; i < size; i++) {
 		var c = i % cols;
 		var r = Math.floor(i / cols);
-		
 		var alive = 0;
 		for (var ii = 0; ii < 9; ii++) {
 			if (ii == 4) {
 				continue;
 			}
-			
 			var x = c + ((ii % 3) - 1);
 			var y = r + (Math.floor(ii / 3) - 1);
-			
 			if (x < 0) x = cols - 1; else if (x == cols) x = 0;
 			if (y < 0) y = rows - 1; else if (y == rows) y = 0;
-			
 			if (tiles[y][x].hasClass("alive")) {
 				alive++;
 			}
 		}
-		
 		var tile = tiles[r][c];
 		if (alive == 3) {
 			tile.addClass("add");
@@ -81,9 +68,8 @@ function step() {
 			tile.addClass("remove");
 		}
 	}
-	
-	game.find(".add").attr("class", "alive");
-	game.find(".remove").attr("class", "");
+	$(".add").attr("class", "alive");
+	$(".remove").attr("class", "");
 }
 
 function pause() {
@@ -103,6 +89,6 @@ function refresh() {
 		}
 		table.append(row);
 	}
+	$("#game").html(table);
 	pause();
-	game.html(table);
 }
